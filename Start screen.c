c1006be5a10cce4b5bd341aaa8696808784909e2
muhammadashiquzzaman06-blip <muhammadashiquzzaman06@gmail.com>
2026-09-07 13:14:44 +0600

    Add files via upload

---
+++ Start screen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(void)
+{
+        printf("=====================================================\n");
+        printf("     THE LORD OF THE RINGS: FEATHER OF DESTINY\n");
+        printf("=====================================================\n");
+        printf("1. Start Game (Play Quest)\n");
+        printf("2. View Game Rules & Hints\n");
+        printf("3. View History Records\n");
+        printf("4. Search Player Records\n");
+        printf("5. Sort Records Table\n");
+        printf("6. Quit Game\n");
+        printf("=====================================================\n");
+
+        return 0;
+}
